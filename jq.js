ecd2c9f5c7c78a454d6dec34b31e6e9274455f17
Hamza6323 <dark83587@gmail.com>
2025-10-27 22:14:50 -0700

    Add files via upload

---
+++ jq.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("p").hover(function() {
+        $(this).css("color", "red");
+    });
+
+    $("#btn").hover(function() {
+        $(this).css({
+            "background-color": "black",
+            "color": "white"
+        });
+    });
+});
